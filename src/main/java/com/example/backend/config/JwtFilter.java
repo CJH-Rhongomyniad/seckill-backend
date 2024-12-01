d2d37f259def61c95160afef1e219f3021331567
CJH-Rhongomyniad <44717106+CJH-Rhongomyniad@users.noreply.github.com>
2024-12-01 09:03:43 +0800

    a

--- src/main/java/com/example/backend/config/JwtFilter.java
+++ src/main/java/com/example/backend/config/JwtFilter.java
@@ -2,17 +2,17 @@ package com.example.backend.config;
 
 import com.example.backend.util.JwtUtil;
 import io.jsonwebtoken.Claims;
-import jakarta.servlet.*;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 
 public class JwtFilter implements Filter {
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException, ServletException {
         System.out.println("doFilter");
         HttpServletRequest httpRequest = (HttpServletRequest) request;
         HttpServletResponse httpResponse = (HttpServletResponse) response;
--- src/main/java/com/example/backend/controller/UserController.java
+++ src/main/java/com/example/backend/controller/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import jakarta.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequest;
 import java.util.HashMap;
 import java.util.Map;
 
